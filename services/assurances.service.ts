265f8a2519123fb7223cc2b10a32c6c692e887f1
A-DRAMANE <abdoulcommedramane@gmail.com>
2023-08-30 15:41:53 +0000

    table name file added, get data desc do

--- services/assurances.service.ts
+++ services/assurances.service.ts
@@ -1,7 +1,8 @@
 import * as oracledb from 'oracledb';
 import { dbConfig } from '../connection/dbConfig';
 import { Assurance } from '../interfaces/assurances/assurance.interface';
-import { responseData, responseObjet } from '../interfaces';
+import { responseData } from '../interfaces';
+import { ASSURANCES } from '../constants/tables';
 
 export const findAll = async (): Promise<responseData | undefined> => {
     try {
@@ -9,7 +10,7 @@ export const findAll = async (): Promise<responseData | undefined> => {
 
         // Configuration pour obtenir un résultat avec les noms de colonnes
         const options = { outFormat: oracledb.OUT_FORMAT_OBJECT };
-        const result = await connection.execute<Assurance>("SELECT * FROM ASSURANCES",[],options);
+        const result = await connection.execute<Assurance>(`SELECT * FROM ${ASSURANCES} order by DATE_INSERT desc`,[],options);
         await connection.close();
 
         return {datas:result.rows,statut:true};
@@ -25,7 +26,7 @@ export const find = async (id:string): Promise<responseData | undefined> => {
 
         // Configuration pour obtenir un résultat avec les noms de colonnes
         const options = { outFormat: oracledb.OUT_FORMAT_OBJECT };
-        const result = await connection.execute<Assurance>(`SELECT * FROM ASSURANCES WHERE NUM_POLICE_ASSURANCE='${id}'`,[],options);
+        const result = await connection.execute<Assurance>(`SELECT * FROM ${ASSURANCES} WHERE NUM_POLICE_ASSURANCE='${id}'`,[],options);
         await connection.close();
 
         return {datas:result.rows,statut:true};
@@ -39,30 +40,30 @@ export const create = async (assurance:Assurance): Promise<responseData | undefi
     try {
         const connection = await oracledb.getConnection(dbConfig);
         const insertQuery = `
-        INSERT INTO ASSURANCES (NUM_POLICE_ASSURANCE, DATE_EFFET, DATE_ECHEANCE, DESTINATION, VILLE, OBSERVATION, MONTANT, ID_USER, NUM_VEHI, NUM_MARCHE, MATRI_EMP, MATRI_FSSEUR, NUM_TYPE) 
-       VALUES ('${assurance.NUM_POLICE_ASSURANCE}', 
-       '${assurance.DATE_EFFET}', 
-       '${assurance.DATE_ECHEANCE}', 
-       ${assurance.DESTINATION?''+assurance.DESTINATION+'':null}, 
+        INSERT INTO ${ASSURANCES} (NUM_POLICE_ASSURANCE, DATE_EFFET, DATE_ECHEANCE, DESTINATION, VILLE, OBSERVATION, MONTANT, ID_USER, NUM_VEHI, NUM_MARCHE, MATRI_EMP, MATRI_FSSEUR, NUM_TYPE) 
+        VALUES ('${assurance.NUM_POLICE_ASSURANCE}', 
+        '${assurance.DATE_EFFET}', 
+        '${assurance.DATE_ECHEANCE}', 
+        ${assurance.DESTINATION?''+assurance.DESTINATION+'':null}, 
         ${assurance.VILLE?''+assurance.VILLE+'':null}, 
-       '${assurance.OBSERVATION}', 
-       ${assurance.MONTANT}, 
-       '${assurance.ID_USER}', 
-       ${assurance.NUM_VEHI?''+assurance.NUM_VEHI+'':null}, 
-       '${assurance.NUM_MARCHE}', 
-       ${assurance.MATRI_EMP?''+assurance.MATRI_EMP+'':null}, 
-       ${assurance.MATRI_FSSEUR}, 
-       ${assurance.NUM_TYPE})`;
-       
-       const options = { outFormat: oracledb.OUT_FORMAT_OBJECT };
-       const result = await connection.execute<Assurance>(insertQuery,[],options);
-       console.log(result);
-       await connection.commit();
+        '${assurance.OBSERVATION}', 
+        ${assurance.MONTANT}, 
+        '${assurance.ID_USER}', 
+        ${assurance.NUM_VEHI?''+assurance.NUM_VEHI+'':null}, 
+        '${assurance.NUM_MARCHE}', 
+        ${assurance.MATRI_EMP?''+assurance.MATRI_EMP+'':null}, 
+        ${assurance.MATRI_FSSEUR}, 
+        ${assurance.NUM_TYPE})`;
+        
+        const options = { outFormat: oracledb.OUT_FORMAT_OBJECT };
+        const result = await connection.execute<Assurance>(insertQuery,[],options);
+
+        await connection.commit();
         await connection.close();
 
         const response = await find(assurance.NUM_POLICE_ASSURANCE)
         return {datas:response?.datas,statut:true, message: "Nouvel élément inséré avec succès"};
-        } catch (error) {
+    } catch (error) {
         console.error("Erreur :", error);
         return { statut:false, message: 'Erreur lors de la récupération des données' }
     }
